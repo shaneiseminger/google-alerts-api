0a842c479ba1a7537a96f39445a0b9e4c4836dee
adasq <asnaebaem@gmail.com>
2019-01-18 19:51:23 +0100

    .

--- test.js
+++ test.js
@@ -5,11 +5,11 @@ function generateCookies(cookie) {
   const getCookieValueByKey = key => cookie.match(new RegExp(`${key}=(.*);Domain`))[1]
 
   const str = JSON.stringify(
-      [
-          { key: 'SID', value: getCookieValueByKey('SID'), domain: 'google.com' },
-          { key: 'HSID', value: getCookieValueByKey('HSID'), domain: 'google.com' },
-          { key: 'SSID', value: getCookieValueByKey('SSID'), domain: 'google.com' },
-      ]
+    [
+      { key: 'SID', value: getCookieValueByKey('SID'), domain: 'google.com' },
+      { key: 'HSID', value: getCookieValueByKey('HSID'), domain: 'google.com' },
+      { key: 'SSID', value: getCookieValueByKey('SSID'), domain: 'google.com' },
+    ]
   );
   console.log('sukces!')
   return new Buffer(str).toString('base64');
@@ -18,44 +18,51 @@ function generateCookies(cookie) {
 const getCookies = async (username, password) => {
   const browser = await puppeteer.launch({
     headless: true,
+    timeout: 5000
   })
-  const page = await browser.newPage();
+  try {
+    const page = await browser.newPage();
 
-  const a = await page.goto('https://accounts.google.com/ServiceLogin');
+    const a = await page.goto('https://accounts.google.com/ServiceLogin');
 
-//identifierId
-  await page.waitForSelector('#Email')
-  let elementHandle = await page.$('#Email');
+    //identifierId
+    await page.waitForSelector('#Email', { timeout: 5000 })
+    let elementHandle = await page.$('#Email');
 
-  console.log('typing username...');
-  await elementHandle.type(username);
-  console.log('pressing enter...');
-  await elementHandle.press('Enter');
-  console.log('waiting for password input');
-  await page.waitForSelector('input[type=password]:focus')
-  elementHandle = await page.$('input[type=password]');
-  console.log('typing password...');
-  await elementHandle.type(password);
-  await elementHandle.press('Enter');
-  console.log('pressing enter...');
-  const challenge = await page.waitForResponse(response => response.url().includes('challenge'))
-  await browser.close();
-  return generateCookies(challenge.headers()['set-cookie'])
+    console.log('typing username...');
+    await elementHandle.type(username);
+    console.log('pressing enter...');
+    await elementHandle.press('Enter');
+    console.log('waiting for password input');
+    await page.waitForSelector('input[type=password]:focus', { timeout: 5000 })
+    elementHandle = await page.$('input[type=password]');
+    console.log('typing password...');
+    await elementHandle.type(password);
+    await elementHandle.press('Enter');
+    console.log('pressing enter...');
+    const challenge = await page.waitForResponse(response => response.url().includes('challenge'))
+    await browser.close();
+    return generateCookies(challenge.headers()['set-cookie'])
+  } catch (e) {
+    console.log(e)
+    await browser.close();
+    return null;
+  }
 }
 
-let [,,username, pswd] = process.argv;
+let [, , username, pswd] = process.argv;
 
 //pswd = ''
 
 console.log(`username: '${username}', pswd: '${pswd}'`)
 
-;(async () => {
-  try {
-    await getCookies(username, pswd)
-  }catch(e) {
-    console.log(e)
-  }
-})()
+  ; (async () => {
+    try {
+      await getCookies(username, pswd)
+    } catch (e) {
+      console.log(e)
+    }
+  })()
 
 
 
